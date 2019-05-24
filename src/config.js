882b2d6c0b51154de86a52184548ce356be0deb9
Charlotte Shih <shih.charlotte@gmail.com>
2019-05-23 21:17:57 -0700

    Try deploying

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-backend',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful-backend',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-backend-test'
 }
